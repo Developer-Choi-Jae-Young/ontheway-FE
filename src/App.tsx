c590d230495b13728e492452b47e190d65375d71
최재영 <cjy951213@naver.com>
2026-09-16 00:50:54 +0900

    feat : 프로필 카드 및 칩 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import CustomButton from './components/CustomButton.tsx'
 import './App.css'
+import CustomProfile from './components/CustomProfile.tsx'
+import CustomChips from './components/CustomChips.tsx'
+import CustomProfileCard from './components/CustomProfileCard.tsx'
 
 function App() {
 
@@ -7,6 +10,11 @@ function App() {
     <>
       <CustomButton name={'활성화'} color={'#fd5d35'} fontColor={'#ffffff'} width={'361px'}></CustomButton>
       <CustomButton name={'활성화'} color={'#4576f7'} fontColor={'#1f2937'} width={'178px'}></CustomButton>
+
+        <CustomProfileCard width={349} nickname='최재영' date='2026-09-15' rateing={3.5} review={24}
+          chipElement={<CustomChips name={'진행중'} color={'#4576F7'} backgroundColor={'#E3EAFD'} height={28}></CustomChips>}
+          profileElement={<CustomProfile width={16} height={20} strok={'#FD5D35'} strokWidth={2} diameter={40} backgroundColor={'#FEF1ED'}></CustomProfile>}>
+      </CustomProfileCard>
     </>
   )
 }
