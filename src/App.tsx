0c3505c7179afff714af0aee38c4a86bdbb27ba0
yezn <188135148+yezjn@users.noreply.github.com>
2026-09-17 12:57:46 +0900

    yejin components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import './App.css'
 import CustomProfile from './components/CustomProfile.tsx'
 import CustomChips from './components/CustomChips.tsx'
 import CustomProfileCard from './components/CustomProfileCard.tsx'
+import TextField from "./components/TextField.tsx"
+import TextArea from "./components/TextArea.tsx"
+import { Info1, Info2 } from './components/Info'
+import TimeInput from './components/TimeInput.tsx'
+import Dropdown from './components/Dropdown.tsx'
+import DateInput from './components/DateInput.tsx'
+import EmptyState from './components/EmptyState.tsx'
 
 function App() {
 
@@ -15,6 +22,46 @@ function App() {
           chipElement={<CustomChips name={'진행중'} color={'#4576F7'} backgroundColor={'#E3EAFD'} height={28}></CustomChips>}
           profileElement={<CustomProfile width={16} height={20} strok={'#FD5D35'} strokWidth={2} diameter={40} backgroundColor={'#FEF1ED'}></CustomProfile>}>
       </CustomProfileCard>
+
+      <TextField height={88} borderColor="lightGray" backgroundColor="white" leftLocationIcon={false} placeholder="내용을 입력해주세요." timer={false} rightButton="none" />
+      <TextField height={88} borderColor="orange" backgroundColor="white" leftLocationIcon={false} placeholder="내용을 입력해주세요." timer={false} rightButton="x"/>
+      <TextField height={88} borderColor="gray" backgroundColor='white' leftLocationIcon={false} placeholder='내용을 입력해주세요.' timer={false} rightButton="none"/>
+      <TextField height={88} borderColor="orange" backgroundColor='white' leftLocationIcon={false} placeholder='내용을 입력해주세요.' timer={false} rightButton="none"/>
+      
+      <TextField height={80} borderColor="lightGray" backgroundColor="white" leftLocationIcon={false} placeholder="Label" timer={true} rightButton="label" />
+      <TextField height={80} borderColor="lightGray" backgroundColor="white" leftLocationIcon={true} placeholder="출발지를 입력해주세요." timer={false} rightButton="none"/>
+      <TextField height={80} borderColor="orange" backgroundColor='white' leftLocationIcon={true} placeholder='출발지를 입력해주세요.' timer={false} rightButton="none"/>
+      <TextField height={80} borderColor="gray" backgroundColor='white' leftLocationIcon={true} placeholder='출발지를 입력해주세요.' timer={false} rightButton="none"/>
+   
+      <TextField height={80} borderColor="lightGray" backgroundColor="white" leftLocationIcon={false} placeholder="내용을 입력해주세요." timer={false} rightButton="none" />
+      <TextField height={80} borderColor="orange" backgroundColor="white" leftLocationIcon={true} placeholder="내용을 입력해주세요." timer={false} rightButton="x"/>
+      <TextField height={80} borderColor="gray" backgroundColor='white' leftLocationIcon={true} placeholder="내용을 입력해주세요." timer={false} rightButton="none"/>
+      <TextField height={80} borderColor="orange" backgroundColor='white' leftLocationIcon={true} placeholder="내용을 입력해주세요." timer={false} rightButton="none"/>
+      <TextField height={80} borderColor="none" backgroundColor='gray' leftLocationIcon={true} placeholder="내용을 입력해주세요." timer={false} rightButton="none"/>
+      
+      <TextArea borderColor="lightGray" textcount={0} />
+      <TextArea borderColor="orange" textcount={1} />
+      <TextArea borderColor="gray" textcount={70} />
+      <TextArea borderColor="orange" textcount={100} />
+
+      <Info1 />
+      <Info2 />
+
+      <TimeInput borderColor="lightGray" backgroundColor="white" />
+      <TimeInput borderColor="none" backgroundColor="gray" />
+      <TimeInput borderColor="orange" backgroundColor="white" />
+
+      <Dropdown borderColor="lightGray" />
+      <Dropdown borderColor="gray" />
+      <Dropdown borderColor="orange" />
+
+      <DateInput borderColor="lightGray" />
+      <DateInput borderColor="orange" />
+      <DateInput borderColor="gray" />
+
+      <EmptyState type="route" />
+      <EmptyState type="photo" />
+      
     </>
   )
 }
---
+++ src/components/TextArea.tsx
@@ -0,0 +1,22 @@
+import './TextArea.css'
+
+//2. props
+type TextAreaProps = {
+  borderColor: 'lightGray' | 'gray' | 'orange'
+  textcount: number
+}
+
+// 1. TextField Component : () 안에 prop를 넣어주면 됨. 
+function TextArea(props : TextAreaProps){
+    return(<div className='text-area' style={{borderColor: props.borderColor}}>
+        <input placeholder="Text"/>
+        <span className='text-count'>
+            <span style={{ color: props.textcount === 100 ? 'orange' : 'gray' }}>
+                {props.textcount}
+            </span>
+            <span style={{ color: 'lightgray' }}>/100</span>
+        </span>
+    </div>)
+}
+
+export default TextArea
