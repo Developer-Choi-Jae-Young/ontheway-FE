ee4bc682426d0fa61b18f4b559b37c3eac517d53
최인하 <xyz030902@gmail.com>
2026-09-20 03:17:56 +0900

    fix: TopAppBar 뒤로가기 아이콘을 꺾쇠 모양으로 변경

--- src/components/CustomTopAppBar.tsx
+++ src/components/CustomTopAppBar.tsx
@@ -12,7 +12,6 @@ function BackArrowIcon() {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="#33363D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M19 12H5" />
             <path d="m12 19-7-7 7-7" />
         </svg>
     )
